112691f60b20fe5a61e4598b942326b95c07a80e
RC_Student_lab <RC_Student_lab@10.0.0.134>
2024-09-18 20:09:42 +0000

    2ND set of changes

--- src/main/java/com/mycompany/login/Login.java
+++ src/main/java/com/mycompany/login/Login.java
@@ -24,8 +24,31 @@ public class Login {
     String username = take_input.nextLine();
     
       System.out.println("Enter your password:");
-    String password = take_input.nextLine();
+    String password = take_input.nextLine();    
+   
+    checkUsername(username);
+    checkPasswordComplexity(password);
     }
     
-   
+ public static void checkUsername(String username){
+  if(username.contains("_" ) && username.length()< 5)
+      System.out.println("Username successfully captured.");
+      
+ else {
+    //error message
+    System.out.println("Username is not correctly formatted, please ensure that your username contains an underscore and is no more than 5 characters in length.");
 }
+}
+ 
+ public static void checkPasswordComplexity(String password){
+     if (password.length()<8 && password.matches(".[A-Z].") && password.matches(".\\d.") && password.matches("^a-zA-Z0=9"))
+         System.out.println("Password successfully captured.");
+         
+         else{
+                 System.out.println("Password is not correctly formatted, please ensure that the password contains at least eight characters, a capital letter, a number and a special character. ");
+                 
+     }
+ 
+ }
+}
+        
